bc0546e6bbc544d8b04bb83a35c8416768baa668
FPMSBTENANG <tenang.fpm@gmail.com>
2026-04-08 11:08:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = "ehasil-v3.4";
+const CACHE_VERSION = "ehasil-v3.5";
 
 const ASSETS = [
   "./",
